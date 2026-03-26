de68f7745e0c98968a56834c7b858514ab3433e0
zzf0113 <joshjj6676@gmail.com>
2026-03-26 20:25:00 +0000

    Update snakeoil3_gym.py

--- snakeoil3_gym.py
+++ snakeoil3_gym.py
@@ -58,9 +58,6 @@ import sys
 import getopt
 import os
 import time
-
-# Windows compatibility: TORCS cannot be launched/killed via shell commands
-_WINDOWS = sys.platform.startswith("win")
 PI= 3.14159265359
 
 data_size = 2**17
@@ -124,7 +121,7 @@ class Client():
         self.vision = vision
 
         self.host= 'localhost'
-        self.port= 3001
+        self.port= 3002
         self.sid= 'SCR'
         self.maxEpisodes=1 # "Maximum number of learning episodes to perform"
         self.trackname= 'unknown'
@@ -538,18 +535,18 @@ def drive_example(c):
     '''This is only an example. It will get around the track but the
     correct thing to do is write your own `drive()` function.'''
     S,R= c.S.d,c.R.d
-    target_speed=300
+    target_speed=250
 
     # Steer To Corner
-    R['steer']= S['angle']*15 / PI
+    R['steer']= S['angle']*10 / PI
     # Steer To Center
-    R['steer']-= S['trackPos']*.10
+    R['steer']-= S['trackPos']*.15
 
     # Throttle Control
     if S['speedX'] < target_speed - (R['steer']*50):
-        R['accel']+= .01
+        R['accel']+= .05
     else:
-        R['accel']-= .01
+        R['accel']-= .05
     if S['speedX']<10:
        R['accel']+= 1/(S['speedX']+.1)
 
@@ -560,21 +557,21 @@ def drive_example(c):
 
     # Automatic Transmission
     R['gear']=1
-    if S['speedX']>50:
-        R['gear']=2
     if S['speedX']>80:
+        R['gear']=2
+    if S['speedX']>100:
         R['gear']=3
-    if S['speedX']>110:
-        R['gear']=4
     if S['speedX']>140:
+        R['gear']=4
+    if S['speedX']>180:
         R['gear']=5
-    if S['speedX']>170:
+    if S['speedX']>200:
         R['gear']=6
     return
 
 # ================ MAIN ================
 if __name__ == "__main__":
-    C= Client(p=3001)
+    C= Client(p=3002)
     for step in range(C.maxSteps,0,-1):
         C.get_servers_input()
         drive_example(C)
